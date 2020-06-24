63277c5598491a702d18fc755bfc16e7b41b423e
Nathan Henniges <demonwolf@demonwolfdev.com>
2020-06-23 20:37:33 -0500

    Moving.

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,34 +1,16 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        share
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div id="main">
+    <Navigation />
   </div>
 </template>
 
 <script>
-export default {}
+import Navigation from '@/components/Shared/Navigation'
+export default {
+  components: {
+    Navigation,
+  },
+}
 </script>
 
 <style>

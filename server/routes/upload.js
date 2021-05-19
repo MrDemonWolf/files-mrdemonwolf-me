eb575abde0b5cfc66219dc24eff88946d69ec8f8
Nathan Henniges <demonwolf@demonwolfdev.com>
2021-05-19 14:57:32 -0500

    [SHARE-62] Fixed typo.

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -42,7 +42,6 @@ router.get('/', requireAuth, isSessionValid, async (req, res) => {
     const totalUploads = uploads.length;
 
     res.status(200).json({ uploads, total: totalUploads });
-
   } catch (e) {
     res.status(500).json({
       code: 'SERVER_ERROR',
@@ -57,61 +56,61 @@ router.get('/', requireAuth, isSessionValid, async (req, res) => {
  * @description Allows a logged in user to upload a file to the server
  */
 router.get('/', requireAuth, isSessionValid, async (req, res) => {
-  try {
-  try {
-    const nanoid32 = customAlphabet(urlFriendyAlphabet, 64);
-    // TODO add validation file check for mineTypes and fileLimit
+    try {
+      const nanoid32 = customAlphabet(urlFriendyAlphabet, 64);
+      // TODO add validation file check for mineTypes and fileLimit
 
-    // Get the body
-    const { stoage, displayName } = req.body;
+      // Get the body
+      const { stoage, displayName } = req.body;
 
-    // Get the files
-    const { name, size, mimetype, mv } = req.files.file;
+      // Get the files
+      const { name, size, mimetype, mv } = req.files.file;
 
-    const tags = JSON.parse(req.body.tags) || [];
-    const extension = path.extname(name);
+      const tags = JSON.parse(req.body.tags) || [];
+      const extension = path.extname(name);
 
-    // Create a random file name using nanoid
-    const fileName = nanoid32();
+      // Create a random file name using nanoid
+      const fileName = nanoid32();
 
-    // Create a random delete key
-    const deleteKey = nanoid32();
+      // Create a random delete key
+      const deleteKey = nanoid32();
 
-    switch (stoage) {
-      default:
-        // eslint-disable-next-line no-case-declarations
-        const filePath = `${path.join(
-          __dirname,
-          '../../public/uploads'
-        )}/${fileName}.${extension}`;
-        // Move the file to a public directory in u folder for express
-        await mv(filePath);
-        break;
-    }
-
-    const upload = new Upload({
-      uploader: req.user.id,
-      displayName: displayName || fileName,
-      fileName,
-      fileType: mimetype,
-      deleteKey,
-      fileSize: size,
-      tags
-    });
-
-    await upload.save();
-    res.status(201).json({
-      file: {
-        name: fileName,
-        size,
-        tags,
-        url: {
-          file: `${process.env.FULL_DOMAIN}/u/${fileName}`,
-          delete: `${process.env.FULL_DOMAIN}/u/${fileName}/delete?key=${deleteKey}`
-        },
-        deleteKey
+      switch (stoage) {
+        default:
+          // eslint-disable-next-line no-case-declarations
+          const filePath = `${path.join(
+            __dirname,
+            '../../public/uploads'
+          )}/${fileName}.${extension}`;
+          // Move the file to a public directory in u folder for express
+          await mv(filePath);
+          break;
       }
-    });
+
+      const upload = new Upload({
+        uploader: req.user.id,
+        displayName: displayName || fileName,
+        fileName,
+        fileType: mimetype,
+        deleteKey,
+        fileSize: size,
+        tags
+      });
+
+      await upload.save();
+      res.status(201).json({
+        file: {
+          name: fileName,
+          size,
+          tags,
+          url: {
+            file: `${process.env.FULL_DOMAIN}/u/${fileName}`,
+            delete: `${process.env.FULL_DOMAIN}/u/${fileName}/delete?key=${deleteKey}`
+          },
+          deleteKey
+        }
+      });
+    }
   } catch (e) {
     res.status(500).json({
       code: 'SERVER_ERROR',
